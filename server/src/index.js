c284a01917181a36575332021808a936f510abf4
KeScript <nnamanikester@gmail.com>
2020-03-26 21:34:10 +0100

    Refactored error handller to middlewares file

--- server/src/index.js
+++ server/src/index.js
@@ -2,11 +2,21 @@ const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
+const middlewares = require("./middlewares");
 
 const app = express();
 app.use(morgan("common"));
 app.use(helmet());
 app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Simple Express Server Setup");
+});
+
+app.use(middlewares.notFound);
+
+app.use(middlewares.errorHandler);
 
 const port = process.env.PORT || 5000;
 
